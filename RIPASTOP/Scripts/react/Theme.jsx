e30cf382772aec6c9cd1779e14c6df111bf0306b
Siavash <sn@DSD>
2018-04-04 07:43:20 -0700

    Fixed IE localstore cahe issues by removing store.js

--- RIPASTOP/Scripts/react/Theme.jsx
+++ RIPASTOP/Scripts/react/Theme.jsx
@@ -2,26 +2,26 @@
 class ThemeToggle extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {           
-            stylePath: '/Content/light.css'           
+        this.state = {
+            stylePath: '/Content/light.css'
         };
         this.toggleTheme = this.toggleTheme.bind(this);
     }
     componentWillMount() {
-        if (store.get('stylePath')) {
+        if (localStorage.getItem('stylePath')) {
             this.setState({ stylePath: store.get('stylePath') });
-        } 
-    }   
+        }
+    }
     toggleTheme() {
 
         if (this.state.stylePath == '/Content/light.css') {
-            var toggle =  '/Content/dark.css'
+            var toggle = '/Content/dark.css'
         } else {
             var toggle = '/Content/light.css'
         }
-       
+
         this.setState({ stylePath: toggle });
-        store.set('stylePath', toggle);
+        localStorage.setItem('stylePath', toggle);
     }
     render() {
         return (
