b76d293c48576345b41244152593848864d69180
Siavash <sn@DSD>
2019-11-07 14:47:08 -0800

    Update gitignore

---
+++ RIPASTOP/Models/StopChangeAudits_PostSubRedact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RIPASTOP.Models
+{
+    public partial class StopChangeAudits
+    {
+        public bool postSubRedact { get; set; }
+    }
+}
---
+++ RIPASTOP/Models/Submissions_Validation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RIPASTOP.Models
+{
+    [MetadataType(typeof(Submissions_Validation))]
+    public partial class Submissions
+    {
+        public Submissions()
+        {
+            statusMsgs = new HashSet<StatusMessage_JSON_vw>();
+        }
+
+        public virtual ICollection<StatusMessage_JSON_vw> statusMsgs { get; set; } //access StatusMessage_JSON_vw entity 
+        public virtual ICollection<Submissions> subList { get; set; }
+
+    }
+    [Bind(Exclude = "ID")]
+    public class Submissions_Validation
+    {
+        [Required]
+        public DateTime? EndDate { get; set; }
+    }
+}
