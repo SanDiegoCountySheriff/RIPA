096c9bf0c0f6b05e21546f2f554acf55e8a4cbf6
shabnamA <38363855+shabnamA@users.noreply.github.com>
2019-12-03 11:12:48 -0800

    Added Migration files

---
+++ RIPASTOP/Migrations/201809252303346_foreignKey.cs
@@ -0,0 +1,18 @@
+namespace RIPASTOP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class foreignKey : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Stops", "SubmissionsID", c => c.Int());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Stops", "SubmissionsID", c => c.Int(nullable: false));
+        }
+    }
+}
